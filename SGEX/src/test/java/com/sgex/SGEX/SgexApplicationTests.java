00966ab214371233f0badfda2235d051e7e6260a
CaioACN <60967502+CaioACN@users.noreply.github.com>
2021-09-15 18:17:24 -0300

    SGEX inicio

---
+++ SGEX/src/test/java/com/sgex/SGEX/SgexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgex.SGEX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
